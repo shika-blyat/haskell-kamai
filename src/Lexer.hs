436c22084505769d58096bae09926ecc30002027
shika-blyat <abdelzighem1@gmail.com>
2020-03-01 19:59:39 +0100

    Create ParserResult type

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -6,28 +6,47 @@ module Lexer
 where
 
 import           Data.Char
+import           Control.Applicative
+
+data ParserError = ParserError{
+    reason :: String,
+    errorBegin :: Int,
+    errorEnd :: Int
+} deriving Show
+
+data ParserResult a = Failure String | Success String a | Error ParserError deriving Show
+
+instance Functor ParserResult where
+    fmap fn (Success s a) = Success s $ fn a
+    fmap _  (Error   pe ) = Error pe
+    fmap _  (Failure s  ) = Failure s
+
+instance Applicative ParserResult where
+    pure = Success ""
+    (Success _ f) <*> a = fmap f a
+    (Error   a  ) <*> _ = Error a
+    (Failure a  ) <*> _ = Failure a
 
 data Token = Token {
-    begin :: Int,
-    end :: Int,
+    tokenBegin :: Int,
+    tokenEnd :: Int,
     atom :: Atom
 } deriving Show
 
 data Atom = AInt Int | ABool Bool | AOp String deriving Show
 
-takeNum :: String -> Int -> Int -> (String, Int)
-takeNum []       num _    = ([], num)
-takeNum (s : xs) num cntr = if isDigit s
-    then takeNum xs (digitToInt s * cntr + num) (cntr * 10)
-    else (s : xs, num)
+takeNum :: String -> Int -> Int -> ParserResult Int
+takeNum [] num _ = Success [] num
+takeNum s@(x : xs) num cntr
+    | isDigit x = takeNum xs (digitToInt x * cntr + num) (cntr * 10)
+    | otherwise = Success s num
 
-consumeNum :: String -> [Atom] -> (String, [Atom])
-consumeNum s atoms = (rem, atoms)  where
-    (rem, num) = takeNum s 0 1
-    atoms      = atoms ++ [AInt num]
+consumeNum :: String -> ParserResult Atom
+consumeNum s = AInt <$> takeNum s 0 1
 
-tokenize :: String -> [Atom] -> [Atom]
-tokenize []       atoms = atoms
-tokenize (s : xs) atoms = if isDigit s
-    then let (rem, atoms) = consumeNum (s : xs) atoms in tokenize rem atoms
-    else atoms
+
+tokenize :: String -> [Atom] -> ParserResult [Atom]
+tokenize []        atoms = Success "" atoms
+tokenize s@(x : _) atoms = if isDigit x
+    then (\atom -> atoms ++ [atom]) <$> consumeNum s
+    else Success "" atoms
